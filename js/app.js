e1854720eced73effe654aead73fc64773d9f3b3
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-04-07 23:05:40 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
+//------ function of get amount of expenses------
 function getAmountOfExpenses(item) {
     const itemExpenseText = document.getElementById(item + '-cost').value;
     const itemExpenseNumber = parseFloat(itemExpenseText);
     return itemExpenseNumber;
 
 }
+// ----- function of get the salary income----
 function getSalary() {
     const salaryText = document.getElementById('total-income').value;
     const salaryNumber = parseFloat(salaryText);
     return salaryNumber;
 
 }
+
+//------ calculation of expenses-------
 document.getElementById('calculate-btn').addEventListener('click', function () {
 
     const foodExpenseNumber = getAmountOfExpenses('food');
@@ -28,6 +32,7 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     remainingBalanceText.innerText = salary - fullCost;
 
 })
+//----savings part js--------
 document.getElementById('save-btn').addEventListener('click', function () {
     const amountOfSavingText = document.getElementById('savings-percent').value;
     const percentageSaving = parseFloat(amountOfSavingText);
